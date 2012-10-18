a75412ca7166c47d1618a92e76d97643c00da3bf
BangL <henno.rickowski@googlemail.com>
2012-10-19 01:34:14 +0200

    We need WorldEdit cleanup

--- com/mewin/WGRegionPermissions/WGRegionPermissionsListener.java
+++ com/mewin/WGRegionPermissions/WGRegionPermissionsListener.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mewin.WGRegionPermissions;
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
--- com/mewin/WGRegionPermissions/WGRegionPermissionsPlugin.java
+++ com/mewin/WGRegionPermissions/WGRegionPermissionsPlugin.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mewin.WGRegionPermissions;
 
 import com.mewin.WGCustomFlags.WGCustomFlagsPlugin;
